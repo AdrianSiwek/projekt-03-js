487c5005a04a03f69292bfbeaab72d2b66dc1928
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-12-29 22:49:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const minValue = 10;
 
 
 const showMsg = () => {
-    if (pass.value.length > minValue || pass.value.match(letters) || pass.value.match(numbers) || pass.value.match(special)) {
+    if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)) {
         p.textContent = "Masz bardzo dobre hasło";
         p.style.color = "lime";
 
-    } else if (pass.value.length > minValue || pass.value.match(letters) || pass.value.match(numbers)) {
+    } else if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)) {
         p.textContent = "Masz dobre hasło";
         p.style.color = "gold";
     } else {
